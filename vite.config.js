45f4f69e605eeb84f735097fc9e2ee0aa9cd435f
Yuan Lee <yuanlee301415@126.com>
2026-01-29 10:51:35 +0800

    chore: development port

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
+import { cwd } from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({mode}) => {
+  const root = cwd()
+  const env = loadEnv(mode, root)
+  console.log('ENV:\n', env)
+  const { VITE_PORT } = env
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    server: {
+      port: Number(VITE_PORT)
+    }
+  }
 })
