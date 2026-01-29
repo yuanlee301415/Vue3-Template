27dc349c2d3fc9e8c5c825ffe1e3d7641858c739
Yuan Lee <yuanlee301415@126.com>
2026-01-29 11:04:52 +0800

    chore: 打印和注入》版本号 & 构建时间

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  const leftStyle =
+    'background-color:#2f353a;color:#fff;padding:0 5px;line-height:1.2rem;border-radius:0.2rem 0 0 0.2rem;'
+  const rightStyle =
+    'background-color:#17a2b8;color:#fff;padding:0 5px;line-height:1.2rem;border-radius:0 0.2rem 0.2rem 0;'
+  console.debug(`%c MODE %c ${import.meta.env.MODE}`, leftStyle, rightStyle)
+  // eslint-disable-next-line no-undef
+  console.debug(`%c VERSION %c ${__APP_VERSION__}`, leftStyle, rightStyle)
+  // eslint-disable-next-line no-undef
+  console.debug(`%c BUILD_TIME %c ${__APP_BUILD_TIME__}`, leftStyle, rightStyle)
+})
+</script>
 
 <template>
   <h1>You did it!</h1>
--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,49 @@ import { cwd } from 'node:process'
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { createHtmlPlugin } from 'vite-plugin-html'
+
+import pkg from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => {
   const root = cwd()
   const env = loadEnv(mode, root)
   console.log('ENV:\n', env)
-  const { VITE_PORT } = env
+  const { VITE_PORT, VITE_INTERNAL_VERSION } = env
+  const __APP_VERSION__ = [pkg.version, VITE_INTERNAL_VERSION].join('.')
+  const __APP_BUILD_TIME__ = new Date().toISOString()
 
   return {
     plugins: [
       vue(),
       vueDevTools(),
+      createHtmlPlugin({
+        entry: 'src/main.js',
+        minify: true,
+        inject: {
+          data: {
+            mode,
+            __APP_VERSION__,
+            __APP_BUILD_TIME__
+          }
+        }
+      })
     ],
+
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       },
     },
+
     server: {
       port: Number(VITE_PORT)
+    },
+
+    define: {
+      __APP_VERSION__: JSON.stringify(__APP_VERSION__),
+      __APP_BUILD_TIME__: JSON.stringify(__APP_BUILD_TIME__)
     }
   }
 })
